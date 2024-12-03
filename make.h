97707443de3dd8e021fc683e233b65a556082feb
shajunxing <shajunxing@163.com>
2024-12-03 04:24:07 +0000

    Add files via upload

--- make.h
+++ make.h
@@ -24,15 +24,14 @@ const enum compiler_type compiler = msvc;
 const enum compiler_type compiler = gcc;
 #endif
 
-// https://stackoverflow.com/questions/2124339/c-preprocessor-va-args-number-of-arguments 兼容msvc
-// _1 ... _63是参数名占位符，最终返回N
-// 0个参数会有问题，参见https://gist.github.com/aprell/3722962，也只能支持gcc/clang
+// https://stackoverflow.com/questions/2124339/c-preprocessor-va-args-number-of-arguments compatible with msvc
+// _1 ... _63 are param name placeholders, finally returns N
+// zero parameter will cause problem, see https://gist.github.com/aprell/3722962 also only support gcc/clang
 #define _expand(x) x
 #define _numargs(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, N, ...) N
 #define numargs(...) _expand(_numargs(_, ##__VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
 
-// 计算多个浮点数的最大值
-// 不用float，因为va_arg需要double，参见 https://stackoverflow.com/questions/11270588/variadic-function-va-arg-doesnt-work-with-float 下同
+// do not use float, because va_arg needs double, see https://stackoverflow.com/questions/11270588/variadic-function-va-arg-doesnt-work-with-float same below
 double _max(int num, ...) {
     double ret = -DBL_MAX;
     va_list args;
@@ -47,9 +46,11 @@ double _max(int num, ...) {
     va_end(args);
     return ret;
 }
+#if defined(max) // see https://stackoverflow.com/questions/4234004/is-maxa-b-defined-in-stdlib-h-or-not
+    #undef max
+#endif
 #define max(...) _max(numargs(__VA_ARGS__), ##__VA_ARGS__)
 
-// 连接字符串，返回值需要手动free
 char *_join(const char *sep, int num, ...) {
     va_list args;
     int i, len;
@@ -74,18 +75,15 @@ char *_join(const char *sep, int num, ...) {
 #define join(sep, ...) _join(sep, numargs(__VA_ARGS__), ##__VA_ARGS__)
 #define concat(...) join("", ##__VA_ARGS__)
 
-// 注意dest应该是已经动态分配的内存
 void append(char **dest, const char *src) {
     *dest = (char *)realloc(*dest, strlen(*dest) + strlen(src) + 1);
     strcat(*dest, src);
 }
 
-// 判断字符串是否相等
 int equals(const char *str1, const char *str2) {
     return strcmp(str1, str2) == 0;
 }
 
-// 判断字符串是否以给定的开头
 int startswith(const char *str, const char *prefix) {
     int len = strlen(str);
     int prefixlen = strlen(prefix);
@@ -95,7 +93,6 @@ int startswith(const char *str, const char *prefix) {
     return strncmp(str, prefix, prefixlen) == 0;
 }
 
-// 判断字符串是否以给定的结尾
 int endswith(const char *str, const char *suffix) {
     int len = strlen(str);
     int suffixlen = strlen(suffix);
@@ -105,25 +102,32 @@ int endswith(const char *str, const char *suffix) {
     return strncmp(str + len - suffixlen, suffix, suffixlen) == 0;
 }
 
+#define run(cmd)                                                          \
+    do {                                                                  \
+        int ret;                                                          \
+        puts(cmd);                                                        \
+        ret = system(cmd);                                                \
+        if (ret != 0) {                                                   \
+            printf("%s:%d: exit code is %d.\n", __FILE__, __LINE__, ret); \
+            exit(ret);                                                    \
+        }                                                                 \
+    } while (0)
+
 enum os_type { windows,
                posix };
 
 #if defined(_WIN32)
 
     #define WIN32_LEAN_AND_MEAN
-    #include <direct.h> // 包含getcwd, chdir, mkdir, rmdir函数
+    #include <direct.h> // includeing functions getcwd, chdir, mkdir, rmdir
     #include <windows.h>
 
-// 操作系统
 enum os_type os = windows;
-// 目录分隔符
 const char *pathsep = "\\";
-// 常用文件扩展名
 const char *objext = ".obj";
 const char *dllext = ".dll";
 const char *exeext = ".exe";
 
-// 返回文件修改时间，UTC秒
 double __mtime(const char *filename) {
     HANDLE fh;
     FILETIME mt;
@@ -138,7 +142,6 @@ double __mtime(const char *filename) {
     return ret;
 }
 
-// 遍历目录，注意dir必须以分隔符结尾，callback(dir, base, ext)，如果是子目录，那么dir就是子目录，base、ext都为NULL，如果是文件，那么dir就是父目录，base、ext都不为NULL，ext带点号，即使没有扩展名，ext也不为NULL，而是""，这样做的好处是可以直接concat(dir, base, ext)得到完整文件名
 void listdir(const char *dir, void (*callback)(const char *, const char *, const char *)) {
     HANDLE sh;
     WIN32_FIND_DATA fd;
@@ -200,7 +203,6 @@ double __mtime(const char *filename) {
 
 #endif
 
-// 返回一个或多个文件的最近修改时间
 double _mtime(int num, ...) {
     double ret = -DBL_MAX;
     va_list args;
